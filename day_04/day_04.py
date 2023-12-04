22290d523824b643ad1883d11b9fa6e984403711
sergiotallotorres <sergiotallotorres@gmail.com>
2023-12-04 11:47:20 +0100

    Finished day 4

--- day_04/day_04.py
+++ day_04/day_04.py
@@ -9,20 +9,21 @@ def stars(starnumber: int, data: list):
     score = 0
     for card_number, card in enumerate(data):
         card = card.split(':')[1].strip()
+
         # regex to find numbers and store the on list
         pattern = r'(\d+)'
         winning_numbers = re.findall(pattern, card.split('|')[0].strip())
         owning_numbers = re.findall(pattern, card.split('|')[1].strip())
-        count = 0
-        for number in owning_numbers:
-            if number in winning_numbers:
-                count += 1
+
+        count = len(set(winning_numbers) & set(owning_numbers))
+
         if starnumber == 1:
             if count > 0:
                 score += 2 ** (count - 1)
         elif starnumber == 2:
             for i in range(count):
                 pile_scratchcards[card_number + i + 1] += 1 * pile_scratchcards[card_number]
+
     if starnumber == 1:
         return score
     elif starnumber == 2:
